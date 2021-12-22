ef4065c0fe060a6d1a2b256d1ec4942284228ce0
technoavengers <navdeep220161@gmail.com>
2021-12-22 10:36:30 +0530

    Few workload added

---
+++ k8s_advance/ingress/ingress-controller.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.0/deploy/static/provider/aws/deploy.yaml
---
+++ k8s_advance/metric-server/metric-setup.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl get deployment metrics-server -n kube-system
