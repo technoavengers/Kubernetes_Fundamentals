2fced38879e42697c3312c1807352b48888de731
technoavengers <navdeep220161@gmail.com>
2022-01-01 15:52:48 +0530

    Blue green deployment added

---
+++ k8s_advance/deployment-strategy/blue-green/start-script.sh
@@ -0,0 +1,7 @@
+kubectl create -f app-configmap.yaml
+kubectl create -f mongo-secret.yaml
+kubectl create -f mongodb-deployment.yaml
+kubectl create -f mongodb-service.yaml
+sleep 10
+kubectl create -f green-deployment.yaml
+kubectl create -f green-service.yaml
---
+++ k8s_advance/deployment-strategy/blue-green/stop-script.sh
@@ -0,0 +1,6 @@
+kubectl delete configmap app-configmap
+kubectl delete secret mongodb-secret
+kubectl delete deployment webapp-green
+kubectl delete deployment mongo
+kubectl delete service mongodb-service
+kubectl delete service green-service
