ea0888769df8c6585648d6435a7d6f0eb7798369
technoavengers <navdeep220161@gmail.com>
2022-01-07 19:22:55 +0530

    Blue green deployment added

--- k8s_advance/kops/kops-setup1.sh
+++ k8s_advance/kops/kops-setup1.sh
@@ -1,9 +1,15 @@
+######## Install Kops ############
+
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+######## Install Kubelet ############
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/kubectl
+
+######## Create Group & add all required access ############
 aws iam create-group --group-name kops
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name kops
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name kops
@@ -12,6 +18,11 @@ aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess -
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name kops
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --group-name kops
 aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --group-name kops
+
+
+######## Create User & add user to above group ############
 aws iam create-user --user-name kops
 aws iam add-user-to-group --user-name kops --group-name kops
 aws iam create-access-key --user-name kops
+
+######## aws configure Add AccessKey/Secret/Region for new user
--- k8s_advance/kops/kops-setup2.sh
+++ k8s_advance/kops/kops-setup2.sh
@@ -1,13 +1,25 @@
+################ Create a aws S3 bucket to store cluster state #########
 aws s3api create-bucket \
     --bucket kops-technoavengers-com-state-store \
     --region us-east-1
+
 aws s3api put-bucket-versioning --bucket kops-technoavengers-com-state-store  --versioning-configuration Status=Enabled
+
+################ Set cluster name and state_store env variable #########
 export NAME=technoavengers.k8s.local
 export KOPS_STATE_STORE=s3://kops-technoavengers-com-state-store
+
+################ Create Kops Cluster Configuration files #########
 kops create cluster \
     --zones=us-east-1a,us-east-1b,us-east-1c \
     --api-loadbalancer-type public \
     ${NAME}
+
+
+
+################ Create key and secret for kops #########
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa
 kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
+
+################ Run your cluster #########
 kops update cluster --name ${NAME} --yes --admin=87600h
